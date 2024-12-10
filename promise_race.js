d149232a36f465056c21df55023f6726b29abbaf
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2024-12-10 09:40:28 +0100

    changed files

--- promise_race.js
+++ promise_race.js
@@ -1,11 +1,11 @@
-const p1 = new Promise((resolve, reject) => {
-    setTimeout(resolve, 500, "Ettan vann!");
-  });
+// const p1 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 500, "Ettan vann!");
+//   });
 
-  const p2 = new Promise((resolve, reject) => {
-    setTimeout(resolve, 100, "Tvåan vann!");
-  });
+//   const p2 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, 100, "Tvåan vann!");
+//   });
 
-  Promise.race([p1, p2]).then(value => {
-    console.log(value);
-  });
+//   Promise.race([p1, p2]).then(value => {
+//     console.log(value);
+//   });
